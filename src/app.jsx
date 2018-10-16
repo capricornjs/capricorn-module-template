4bc691e28a2f66ded56c69faff0b33c8c1247d62
yangxi <599321378@qq.com>
2018-10-16 17:16:18 +0800

    feat: 给跟节点添加className和role属性

--- src/app.jsx
+++ src/app.jsx
@@ -2,10 +2,11 @@ import Home from 'views/home'
 
 import 'style/app.less'
 
-let id = '{{ moduleName }}'
-id = id + '-' + Date.now()
+const moduleName = '{{ moduleName }}'
+const id = moduleName + '-' + Date.now()
 const wrapContainer = document.createElement('div')
 wrapContainer.id = id
+wrapContainer.className = wrapContainer.role = moduleName
 document.body.append(wrapContainer)
 
 ReactDOM.render(<Home />, document.getElementById(id))
