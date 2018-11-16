e2f04620b8f97f9b2153a24dc78f6ad9e2dcd0fb
yangxi <599321378@qq.com>
2018-11-16 19:55:25 +0800

    feat: 更新模版

---
+++ template/lib/index.js
@@ -0,0 +1,6 @@
+(function () {
+	!window.Capricorn && (window.Capricorn = {})
+	window.Capricorn.modules = {}
+	window.Capricorn.modules['capricorn-module'] = []
+	window.Capricorn.modules['capricorn-module'].push({ 'name': 'capricorn-module' })
+})()
