16e24ccd43971bebc509e63dbf1537ff46d43560
yangxi <599321378@qq.com>
2018-11-16 19:58:20 +0800

    feat: 更新模版

--- template/lib/index.js
+++
@@ -1,6 +0,0 @@
-(function () {
-	!window.Capricorn && (window.Capricorn = {})
-	window.Capricorn.modules = {}
-	window.Capricorn.modules['capricorn-module'] = []
-	window.Capricorn.modules['capricorn-module'].push({ 'name': 'capricorn-module' })
-})()
