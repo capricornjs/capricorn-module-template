37eb0e48ae9200c914eea9729e950c697fdf5e2e
yangxi <599321378@qq.com>
2018-11-17 15:29:48 +0800

    fix: 修复一个拿取配置数据的bug

--- src/app.jsx
+++ src/app.jsx
@@ -7,7 +7,7 @@ import configData from '../module.config.json'
 
 const moduleData = window.Capricorn.modules['{{ moduleName }}'][0]
 const moduleName = moduleData.name
-store.setData(moduleName.config || configData)
+store.setData(moduleData.config || configData)
 window.Capricorn.modules['{{ moduleName }}'].shift()
 const id = moduleName
 const wrapContainer = document.createElement('div')
